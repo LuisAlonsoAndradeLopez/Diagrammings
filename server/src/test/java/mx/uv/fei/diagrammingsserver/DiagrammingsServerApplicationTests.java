0bdd7c0a6bbc6d8f580b1b4637459ddd8c8bd0b6
LuisAlonsoAndradeLopez <caffeinated340@gmail.com>
2024-12-19 23:56:10 -0600

    Spring boot server base files

---
+++ server/src/test/java/mx/uv/fei/diagrammingsserver/DiagrammingsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uv.fei.diagrammingsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagrammingsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
